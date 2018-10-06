faea4f31212cc542333fc37cf1daa62f1fe19eba
Mihalyfi Janos <janos.mihalyfi@schibsted.com>
2018-10-06 13:07:22 +0200

    Fix array indexes

--- src/PokerLogic.php
+++ src/PokerLogic.php
@@ -9,8 +9,8 @@ class PokerLogic {
 
 	public function __construct($game_state) {
 		$this->GameState = new GameState($game_state);
-		$this->CardChecker = new CardChecker($game_state[$game_state['in_action']]['hole_cards'], $game_state['community_cards']);
-		$this->PokerPlayer = new PokerPlayer($game_state[$game_state['in_action']]);
+		$this->CardChecker = new CardChecker($game_state['players'][$game_state['in_action']]['hole_cards'], $game_state['community_cards']);
+		$this->PokerPlayer = new PokerPlayer($game_state['players'][$game_state['in_action']]);
 
 	}
 
