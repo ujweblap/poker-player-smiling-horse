6aef59d0dae82aafb6878d36dcdd889afc4d0e65
Berényi Bence <bbence@schibsted.hu>
2018-10-06 11:40:15 +0200

    PokerLogic

--- src/PokerLogic.php
+++ src/PokerLogic.php
@@ -3,9 +3,14 @@
 namespace SmilingHorse;
 
 class PokerLogic {
+	public $GameState;
+	public $CardChecker;
+	public $PokerPlayer;
 
-
-	public function __construct() {
+	public function __construct($game_state) {
+		$this->GameState = new GameState($game_state);
+		$this->CardChecker = new CardChecker($game_state[$game_state['in_action']]['hole_cards'], $game_state['community_cards']);
+		$this->PokerPlayer = new PokerPlayer($game_state[$game_state['in_action']]);
 
 	}
 
