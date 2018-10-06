27ed4a2ec259ef7c984d354aab1e1f0e94df7ec5
Pörös Dávid <david.poros@schibsted.com>
2018-10-06 11:29:43 +0200

    Add test for Card Checker^

--- test/CardCheckerTest.php
+++ test/CardCheckerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use SmilingHorse\CardChecker;
 
 class CardCheckerTest extends TestCase {
     protected $cardChecker;
@@ -8,11 +9,85 @@ class CardCheckerTest extends TestCase {
     public function setUp()
     {
         parent::setUp();
-
     }
 
     public function testHasPair()
     {
+        $cardChecker = new \SmilingHorse\CardChecker(
+            [
+                [
+                    'rank' => '6',
+                    'suit' => 'hearts',
+                ],
+                [
+                    'rank' => '6',
+                    'suit' => 'spades',
+                ],
+            ],
+            [
+                [
+                    'rank' => '8',
+                    'suit' => 'clubs',
+                ],
+                [
+                    'rank' => '9',
+                    'suit' => 'spades',
+                ],
+                [
+                    'rank' => '10',
+                    'suit' => 'spades',
+                ],
+                [
+                    'rank' => '4',
+                    'suit' => 'spades',
+                ],
+                [
+                    'rank' => 'A',
+                    'suit' => 'spades',
+                ],
+            ]
+        );
 
+        $this->assertEquals(CardChecker::PAIR, $cardChecker->getWhatWeHave());
+    }
+
+    public function testHasTwoPair()
+    {
+        $cardChecker = new \SmilingHorse\CardChecker(
+            [
+                [
+                    'rank' => '6',
+                    'suit' => 'hearts',
+                ],
+                [
+                    'rank' => '6',
+                    'suit' => 'spades',
+                ],
+            ],
+            [
+                [
+                    'rank' => '8',
+                    'suit' => 'clubs',
+                ],
+                [
+                    'rank' => '8',
+                    'suit' => 'spades',
+                ],
+                [
+                    'rank' => '10',
+                    'suit' => 'spades',
+                ],
+                [
+                    'rank' => '4',
+                    'suit' => 'spades',
+                ],
+                [
+                    'rank' => 'A',
+                    'suit' => 'spades',
+                ],
+            ]
+        );
+
+        $this->assertEquals(CardChecker::TWO_PAIR, $cardChecker->getWhatWeHave());
     }
 }
