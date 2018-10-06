09f73b0e377ee33d912606d2fd6d47d03de5a72d
Pörös Dávid <david.poros@schibsted.com>
2018-10-06 10:47:12 +0200

    Check for one and two pairs

---
+++ src/CardChecker.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SmilingHorse;
+
+class CardChecker
+{
+    protected $handCards;
+    protected $communityCards;
+    protected $allCards;
+
+    const NOTHING = 0;
+    const PAIR = 1;
+    const TWO_PAIR = 2;
+
+    /**
+     * CardChecker constructor.
+     * @param $handCards
+     * @param $communityCards
+     */
+    public function __construct($handCards, $communityCards)
+    {
+        $this->handCards = $handCards;
+        $this->communityCards = $communityCards;
+        $this->allCards = array_merge($handCards, $communityCards);
+    }
+
+    public function getWhatWeHave()
+    {
+        $weHave = static::NOTHING;
+
+        $firstPair = $this->hasPair();
+        if($firstPair !== false) {
+            if($this->hasPair($firstPair) !== false)
+            {
+                $weHave = static::TWO_PAIR;
+            } else {
+                $weHave = static::PAIR;
+            }
+        }
+
+        return $weHave;
+    }
+
+    public function hasPair($expectRank = false)
+    {
+        foreach ($this->allCards as $i => $card) {
+            foreach ($this->allCards as $i2 => $card2) {
+                if ($i !== $i2) {
+                    if ($card['rank'] == $card2['rank'] && (!$expectRank || $card['rank'] != $expectRank)) {
+                        return $card['rank'];
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
