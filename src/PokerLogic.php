bbc0f3ec7ea52085fbf5b1957efe298d0ff24654
Mihalyfi Janos <janos.mihalyfi@schibsted.com>
2018-10-06 11:54:45 +0200

    Add switch for deciding what will we do

--- src/PokerLogic.php
+++ src/PokerLogic.php
@@ -14,8 +14,31 @@ class PokerLogic {
 
 	}
 
+	const STRAIGHT = 4;
+	const FLUSH = 5;
+	const FULL_HOUSE = 6;
+	const FOUR_OF_A_KIND = 7;
+	const STRAIGHT_FLUSH = 8;
+	const ROYAL_FLUSH = 9;
 	public function getBet() {
 		$to_bet = $this->GameState->getCurrentBuyIn() + $this->GameState->getPlayers()[$this->GameState->getInAction()]->getBet();
+
+		switch ($this->CardChecker->getWhatWeHave()) {
+			case CardChecker::NOTHING:
+			case CardChecker::PAIR:
+			case CardChecker::TWO_PAIR:
+			case CardChecker::DRILL:
+			case CardChecker::STRAIGHT:
+			case CardChecker::FLUSH:
+			case CardChecker::FULL_HOUSE:
+			case CardChecker::FOUR_OF_A_KIND:
+			case CardChecker::STRAIGHT_FLUSH:
+			case CardChecker::ROYAL_FLUSH:
+
+			default:
+				return 0;
+		}
+
 		return 0;
 	}
 }
