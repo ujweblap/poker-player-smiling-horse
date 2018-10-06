e4d6dd7737aa8b50827590a13117f568464d7972
Berényi Bence <bbence@schibsted.hu>
2018-10-06 10:14:17 +0200

    check 9 or higher cards to allin logic, turned OFF

--- player.php
+++ player.php
@@ -6,7 +6,30 @@ class Player
 
     public function betRequest($game_state)
     {
-        return 10000;
+    	//ALL IN
+	    return 10000;
+
+    	$all_in = $this->checkCards($game_state[$game_state['in_action']]['hole_cards'], $game_state['community_cards']);
+    	if ($all_in) {
+		    return 10000;
+	    } else {
+    		return 0;
+	    }
+    }
+
+    public function checkCards($own_cards, $community_cards) {
+		$cards = array_merge($own_cards, $community_cards);
+		if ($this->check9orHigher($own_cards)) {
+			return true;
+		}
+		return true;
+    }
+
+    public function check9orHigher($own_cards) {
+    	if ($own_cards[0]['rank'] >= 9 && $own_cards[1]['rank'] >= 9) {
+    		return true;
+	    }
+    	return false;
     }
 
     public function showdown($game_state)
